8220e6a5870582a92c1b1a274e2a20f0fc556653
Akmoilt <akmoilt@gmail.com>
2022-05-03 15:12:14 +0300

    Move fixtures into conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,49 @@
+import logging
+import os
+import random
+from pathlib import Path
+from typing import Generator
+
+import pymavlink.mavutil
+import pytest
+
+from drones.drone_client import DroneClient
+from drones.drone_daemon import DroneDaemon
+from drones.testing import TcpSerialConnectionDef, simulation_context
+
+@pytest.fixture
+def mavlink_connection(tmpdir: str) -> pymavlink.mavutil.mavfile:
+    port = random.randrange(5900, 6100)
+    logging.info(f"Using random tcp port {port} for simulation connection")
+    serial_ports_override = {}
+
+    external_mavproxy_port = os.environ.get("MAVPROXY_PORT")
+    if external_mavproxy_port is not None and len(external_mavproxy_port) > 0:
+        external_mavproxy_connection = TcpSerialConnectionDef(
+            port=int(external_mavproxy_port), wait_for_connection=True
+        )
+        serial_ports_override[1] = external_mavproxy_connection
+
+    serial_ports_override[0] = TcpSerialConnectionDef(
+        port=port, wait_for_connection=True
+    )
+    with simulation_context(
+        cwd=Path(tmpdir),
+        serial_ports_override=serial_ports_override,
+    ) as sim:
+        mavlink = sim.mavlink_connect_to_serial(0)
+        mavlink.wait_heartbeat(timeout=500)
+        logging.debug("Connected successfully to simulation, received heartbeat.")
+        yield mavlink
+
+
+@pytest.fixture(scope="function")
+def sim_drone(
+    mavlink_connection: pymavlink.mavutil.mavfile,
+) -> Generator[DroneClient, None, None]:
+    daemon = DroneDaemon(mavlink_connection=mavlink_connection)
+    client = daemon.create_client()
+    try:
+        yield client
+    finally:
+        del daemon
--- tests/test_drone_controller.py
+++ tests/test_drone_controller.py
@@ -1,51 +1,12 @@
 import enum
-import logging
-import os
-from pathlib import Path
-import random
-from typing import Generator
-import pytest
 
 from async_state_machine import StateMachine, State
 from async_state_machine.transitions.timeout import timeout
 from drones.commands import FlightMode
 
 from drones.drone_client import DroneClient
-from drones.drone_daemon import DroneDaemon
-from drones.testing import TcpSerialConnectionDef, simulation_context
 
 from tests.state_machine_utils import run_until
-
-
-@pytest.fixture(scope="function")
-def sim_drone(tmpdir: str) -> Generator[DroneClient, None, None]:
-    port = random.randrange(5900, 6100)
-    logging.info(f"Using random tcp port {port} for simulation connection")
-    serial_ports_override = {}
-
-    external_mavproxy_port = os.environ.get("MAVPROXY_PORT")
-    if external_mavproxy_port is not None and len(external_mavproxy_port) > 0:
-        external_mavproxy_connection = TcpSerialConnectionDef(
-            port=int(external_mavproxy_port), wait_for_connection=True
-        )
-        serial_ports_override[1] = external_mavproxy_connection
-
-    serial_ports_override[0] = TcpSerialConnectionDef(
-        port=port, wait_for_connection=True
-    )
-    with simulation_context(
-        cwd=Path(tmpdir),
-        serial_ports_override=serial_ports_override,
-    ) as sim:
-        mavlink = sim.mavlink_connect_to_serial(0)
-        mavlink.wait_heartbeat(timeout=500)
-        logging.debug("Connected successfully to simulation, received heartbeat.")
-        daemon = DroneDaemon(mavlink_connection=mavlink)
-        client = daemon.create_client()
-        try:
-            yield client
-        finally:
-            del daemon
 
 
 def test_drone_heartbeat(sim_drone: DroneClient) -> None:
@@ -151,6 +112,3 @@ def test_drone_takeoff(sim_drone: DroneClient) -> None:
     )
 
     run_until(sm, target=StateNames.IN_THE_AIR, error_states={StateNames.ERROR})
-
-
-    assert sm.current_state.name == StateNames.IN_THE_AIR
