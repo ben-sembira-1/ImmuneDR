94b5f0a31bc1da39bb401a7c444d6c54bf4bda1f
Akmoilt <akmoilt@gmail.com>
2022-05-08 16:49:21 +0300

    Add tests for land(), turn() and change_altitude()

--- tests/test_drone_controller.py
+++ tests/test_drone_controller.py
@@ -73,6 +73,32 @@ def test_drone_takeoff(sim_drone: DroneClient) -> None:
     )
 
 
+def test_land(flying_sim_drone: DroneClient):
+    # TODO start at different heights
+    # TODO check that drone is disarmed
+    # TODO test when GPS is inactive
+    @enum.unique
+    class StateNames(enum.Enum):
+        LANDING = "Landing"
+        DONE = "Landed"
+        ERROR = "Error"
+
+    sm = StateMachine(
+        [
+            State(
+                name=StateNames.LANDING,
+                transitions={
+                    StateNames.DONE: flying_sim_drone.land(),
+                    StateNames.ERROR: timeout(secs=10),
+                },
+            ),
+            State(name=StateNames.DONE, transitions={}),
+            State(name=StateNames.ERROR, transitions={}),
+        ]
+    )
+    run_until(sm, target=StateNames.DONE, error_states={StateNames.ERROR})
+
+
 def test_disable_gps(flying_sim_drone: DroneClient, mavlink_connection):
     @enum.unique
     class StateNames(enum.Enum):
@@ -96,3 +122,56 @@ def test_disable_gps(flying_sim_drone: DroneClient, mavlink_connection):
 
     disable_gps(mavlink_connection)
     run_until(sm, target=StateNames.NO_GPS, error_states={StateNames.ERROR})
+
+
+def test_turn(flying_sim_drone: DroneClient):
+    # TODO parametrize with different start and target headings
+    # TODO test illegal heading (out of range)
+    # TODO test when GPS is inactive
+    @enum.unique
+    class StateNames(enum.Enum):
+        TURNING = "Turning"
+        DONE = "Done turning"
+        ERROR = "Error"
+
+    sm = StateMachine(
+        [
+            State(
+                name=StateNames.TURNING,
+                transitions={
+                    StateNames.DONE: flying_sim_drone.turn(heading=0),
+                    StateNames.ERROR: timeout(secs=5),
+                },
+            ),
+            State(name=StateNames.DONE, transitions={}),
+            State(name=StateNames.ERROR, transitions={}),
+        ]
+    )
+
+    run_until(sm, target=StateNames.DONE, error_states={StateNames.ERROR})
+
+
+def test_change_altitude(flying_sim_drone: DroneClient):
+    # TODO parametrize with different start and target altitudes
+    # TODO test when GPS is inactive
+    @enum.unique
+    class StateNames(enum.Enum):
+        CHANGING_ALTITUDE = "Changing altitude"
+        DONE = "Reached target altitude"
+        ERROR = "Error"
+
+    sm = StateMachine(
+        [
+            State(
+                name=StateNames.CHANGING_ALTITUDE,
+                transitions={
+                    StateNames.DONE: flying_sim_drone.change_altitude(height_m=20),
+                    StateNames.ERROR: timeout(secs=10),
+                },
+            ),
+            State(name=StateNames.DONE, transitions={}),
+            State(name=StateNames.ERROR, transitions={}),
+        ]
+    )
+
+    run_until(sm, target=StateNames.DONE, error_states={StateNames.ERROR})
