f0d06808b5b4ea9db1aeeaa59afd3cf90d73b5d0
Akmoilt <akmoilt@gmail.com>
2022-05-15 14:02:35 +0300

    Delete ChangeAltitude (this was implemented with the wrong command)

--- src/drones/commands.py
+++ src/drones/commands.py
@@ -86,40 +86,6 @@ class Takeoff(Command):
         )
 
 
-class ChangeAltitude(Command):
-    def __init__(self, height_m: float, alt_rate_of_change: float = 10.0) -> None:
-        """
-        Change target altitude at a given rate. This slews the vehicle at a controllable rate between its previous
-        altitude and the new one. (affects GUIDED only. Outside GUIDED, aircraft ignores these commands.)
-
-        :param height_m: Target height in meters
-        :param alt_rate_of_change: Rate of change, towards new altitude. 0 for maximum rate change.
-        """
-        super().__init__()
-        self.height_m = height_m
-        assert (
-            alt_rate_of_change >= 0
-        ), "Rate of change must be positive, negative numbers will not converge to target altitude!"
-        self.alt_rate_of_change = alt_rate_of_change
-
-    def __call__(self, mavlink_connection: mavfile) -> None:
-        logging.info(f"Ascending/descending to altitude {self.height_m}")
-        mav: MAVLink = mavlink_connection.mav
-        mav.command_long_send(
-            target_system=mavlink_connection.target_system,
-            target_component=mavlink_connection.target_component,
-            command=MAV_CMD_GUIDED_CHANGE_ALTITUDE,
-            confirmation=0,
-            param1=0.0,
-            param2=0.0,
-            param3=self.alt_rate_of_change,
-            param4=0.0,
-            param5=0.0,
-            param6=0.0,
-            param7=self.height_m,
-        )
-
-
 class SetAttitude(Command):
     HOLD_ALTITUDE_THRUST = (
         0.5  # Setting the thrust to this value tells holds a constant altitude
